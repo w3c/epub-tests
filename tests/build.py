06797110cc3d41efecfc8ad6bf20d59052a073c6
dauwhe <dauwhe@gmail.com>
2020-12-04 17:48:24 -0500

    continued work on python script

--- tests/build.py
+++ tests/build.py
@@ -1,6 +1,8 @@
 # lets play around with python3
 import os, sys, shutil
 from bs4 import BeautifulSoup
+from lxml import etree
+from lxml.html.soupparser import fromstring
 
 # where am I?
 dir_path = os.path.dirname(os.path.realpath(__file__))
@@ -9,6 +11,7 @@ dir_path = os.path.dirname(os.path.realpath(__file__))
 
 
 filename, file_extension = os.path.splitext(sys.argv[1])
+
 
 
 # create directory for our test EPUB
@@ -31,6 +34,7 @@ generic_package_file = 'common/package.opf'
 generic_container_file = 'common/container.xml'
 generic_mimetype_file = 'common/mimetype'
 generic_nav_file = 'common/nav.xhtml'
+generic_content_file = 'common/content_001.xhtml'
 
 # where the generic files should go
 
@@ -42,9 +46,49 @@ new_content_file = os.path.join(test_folder_name, 'OPS', 'content_001.xhtml')
 
 # do the copying 
 
-shutil.copy2(generic_package_file, new_package_file)
+if file_extension == '.html':
+  shutil.copy2(generic_package_file, new_package_file)
+  # copy the original HTML file 
+  shutil.copy2(sys.argv[1], new_content_file)
+  with open(new_content_file) as fp:
+   soup = BeautifulSoup(fp, 'html.parser')
+   
+   assert_statement = soup.find("meta", {"name":"assert"})['content']
+   test_title = soup.title.string
+  
+   print(test_title)
+   
+  
+   
+
+if file_extension == '.opf':
+  shutil.copy2(generic_content_file, new_content_file)
+  # copy the original Package file 
+  shutil.copy2(sys.argv[1], new_package_file)
+  
+  # read info from package file using the xml parser
+  with open(new_package_file) as fp:
+   package_file = BeautifulSoup(fp, 'lxml')
+   test_statement = package_file.find("meta", {"name":"test"})['content']
+   assert_statement = package_file.find("meta", {"name":"assert"})['content']
+   print(test_statement)
+   with open (new_content_file) as lp:
+     content_file =  BeautifulSoup(lp, 'html.parser')
+     content_file.p.string = test_statement
+     new_assert_meta = content_file.new_tag('meta', content=assert_statement) #need to work on getting 'name' attribute too but it's complicated because of Soup reserving "name"
+     print(new_assert_meta)
+     # content_file.meta.append(new_assert_meta)
+     
+     print(content_file.p)
+     # need to save our changes... could this be easier
+     new_html = content_file.prettify("utf-8")
+     lp.close()
+     with open (new_content_file, "wb") as file:
+       file.write(new_html)
+     
+  
 shutil.copy2(generic_container_file, new_container_file)
 shutil.copy2(generic_mimetype_file, new_mimetype_file)
 shutil.copy2(generic_nav_file, new_nav_file)
-# copy the original HTML file 
-shutil.copy2(sys.argv[1], new_content_file)
+
+
