5473da87de70154a319be532395043834ac82dcd
Ivan Herman <ivan@ivan-herman.net>
2021-10-28 15:35:03 +0200

    Reorganized the code to clearly separate the configuration handling from the rest

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -4,48 +4,47 @@
  *  @packageDocumentation
  */
 
-import { ImplementationTable, Constants, Config } from './types';
-
+import { ImplementationTable, Constants, ReportData } from './types';
 import * as fs from "fs";
 
 /**
- * Read the configuration JSON file and convert it into the configuration object.
- * 
- * @returns the configuration file content.
+ * External configuration type
  */
-export function get_config(): Config {
-    try {
-        const config_text = fs.readFileSync(Constants.CONFIG_FILE, 'utf-8');
-        return <Config>JSON.parse(config_text);    
-    } catch (error) {
-        console.error("Warning! The configuration file does not exist or is incorrect!")
-        return {
-            final             : true,
-            coverage_labels   : [],
-            document_mappings : [],
-        }
-    }
+interface Config {
+    final: boolean;
+    coverage_labels: string[];
+    document_mappings: { from: string; to: string}[];
 }
 
 /**
  * If the configuration file sets the `final` value to `false`, all references to the final,`/TR` version of the document must be changed for the editor's draft. This function changes those references.
  * 
  * @param config Configuration file
- * @param url incoming URL
+ * @param table reference to all the tests (as part of the implementation table)
  * @returns new URL
  */
-export function change_doc_references(config: Config, url: string): string {
-    if (config.final === false) {
-        for (const mapping of config.document_mappings) {
-            if (url.startsWith(mapping.from)) {
-                return url.replace(mapping.from, mapping.to);
+function change_references(config: Config, table: ImplementationTable[]): ImplementationTable[] {
+    const change_doc_references = (url: string): string => {
+        if (config.final === false) {
+            for (const mapping of config.document_mappings) {
+                if (url.startsWith(mapping.from)) {
+                    return url.replace(mapping.from, mapping.to);
+                }
             }
+            return url;
+        } else {
+            return url;
         }
-        return url;
-    } else {
-        return url;
     }
+
+    for (const implementation_table of table) {
+        for (const implementation_data of implementation_table.implementations) {
+            implementation_data.references = implementation_data.references.map(change_doc_references);
+        }
+    }
+    return table;
 }
+
 
 /**
  * The implementation table array is sorted using the `header` member as a key, and following the order of the coverage labels
@@ -56,7 +55,7 @@ export function change_doc_references(config: Config, url: string): string {
  * @param table Properly sorted table
  * @returns 
  */
-export function order_implementation_table(config: Config, table: ImplementationTable[]): ImplementationTable[] {
+function order_implementation_table(config: Config, table: ImplementationTable[]): ImplementationTable[] {
     const simple_sort = (left: number|string, right: number|string): number => {
         if (left < right) return -1;
         else if (left > right) return 1;
@@ -84,4 +83,40 @@ export function order_implementation_table(config: Config, table: Implementation
     });
 
     return [...final_mentioned, ...final_not_mentioned];
+}
+
+/**
+ * Apply the configuration actions on the report before it is used to generate HTML files.
+ * 
+ * At present this means:
+ * 
+ * 1. Update the document references to the editor's drafts if the report is not final
+ * 2. Use the order of sections listed in the configuration for the generated tables
+ * 
+ * @param report 
+ * @returns 
+ */
+export function apply_configuration_options(report: ReportData): ReportData {
+    try {
+        // 1. Get the configuration file. If there are issues, just return the original data, unchanged.
+        const config_text = fs.readFileSync(Constants.CONFIG_FILE, 'utf-8');
+        const config: Config = <Config>JSON.parse(config_text); 
+
+        // 2. get the document references right
+        change_references(config, report.tables);
+
+        // 3. sort the implementation tables
+        const tables = order_implementation_table(config, report.tables);
+        const consolidated_tables = order_implementation_table(config, report.consolidated_tables);
+
+        return {
+            tables,
+            consolidated_tables,
+            implementers              : report.implementers,
+            consolidated_implementers : report.consolidated_implementers,
+        }
+    } catch (error) {
+        console.log(error);
+        return report;
+    }
 }
