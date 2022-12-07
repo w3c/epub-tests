530b23821e843895c241a1b7e113dfd64f3e05fd
Ivan Herman <ivan@ivan-herman.net>
2022-12-07 08:29:07 +0100

    Improved test reports (bis)

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { argv } from "process";
 import * as fs_old_school from "fs";
 const fs = fs_old_school.promises;
 
-import { TestData, ReportData, Raw_ImplementationReport, Constants } from './lib/types';
+import { TestData, ReportData, Raw_ImplementationReport, Constants, HTMLFragments } from './lib/types';
 import { get_test_data, get_report_data, get_template } from "./lib/data";
 import { create_report } from "./lib/html";
 import { apply_configuration_options } from './lib/config';
@@ -30,17 +30,13 @@ async function main() {
     const template: Raw_ImplementationReport = get_template(report_data);
 
     const final_report_data: ReportData = apply_configuration_options(report_data);
-    const { implementations, results, tests, creators }: {
-            implementations: string, 
-            results:string, 
-            tests: string, 
-            creators: string
-        } = create_report(final_report_data);
+    const { implementations, consolidated_results, complete_results, tests, creators }: HTMLFragments = create_report(final_report_data);
     const opds_data: OPDS = create_opds(test_data);
     
     await Promise.all([
         fs.writeFile(Constants.IMPL_FRAGMENT, implementations, 'utf-8'),
-        fs.writeFile(Constants.RESULT_FRAGMENT, results, 'utf-8'),
+        fs.writeFile(Constants.CONSOLIDATED_RESULT_FRAGMENT, consolidated_results, 'utf-8'),
+        fs.writeFile(Constants.COMPLETE_RESULT_FRAGMENT, complete_results, 'utf-8'),
         fs.writeFile(Constants.TEST_FRAGMENT, tests, 'utf-8'),
         fs.writeFile(Constants.CREATORS_FRAGMENT, creators, 'utf-8'),
         fs.writeFile(Constants.TEST_RESULTS_TEMPLATE, JSON.stringify(template, null, 4)),
